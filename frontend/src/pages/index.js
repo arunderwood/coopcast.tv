bab22f1b04eec809c2cd3b3022e56f9690a89a62
arunderwood <arunderwood@users.noreply.github.com>
2023-05-06 15:02:23 -0700

    Embed video

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,91 +1,39 @@
+import "./styles.css"
+
 import * as React from "react"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+import YouTube from "react-youtube"
+import { Helmet } from "react-helmet"
 
 const IndexPage = () => {
+  const videoOptions = {
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
+  const emojiFavicon = `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y="1em" font-size="100" font-family="sans-serif">🐔</text></svg>`
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
+    <main className="page">
+      <Helmet>
+        <link rel="icon" href={emojiFavicon} />
+      </Helmet>
+      <h1 className="heading">
         CoopCast.tv
         <br />
-        <span style={headingAccentStyles}>— 🐔🐔🐔</span>
+        <span className="heading-accent">🐔🐔🐔</span>
       </h1>
-      <p style={paragraphStyles}>
-        Coming May 2023 🐣
-      </p>
+      <div className="video-container">
+        <YouTube
+          videoId="Va4AWbTcJWY"
+          opts={videoOptions}
+          className="video"
+        />
+      </div>
     </main>
   )
 }
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>coopcast.tv</title>
